867e2744294c0ec6bf6a6b36e52385b94a866848
Davi Slm <davisoaresdasilva1203@gmail.com>
2023-08-17 19:21:40 -0300

    Questões

--- Exercícios Sequencial/Q18.py
+++ Exercícios Sequencial/Q18.py
@@ -1,9 +1,13 @@
 '''Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).'''
 
-arquivo = float(input("Digite o tamanho do arquivo em Megabytes:"))
+# Receba o tamanho do arquivo em Megabytes
+arquivo = float(input("Digite o tamanho do arquivo em Mb:"))
+
+# Receba a velocidade de Internet em Megabytes por segundo
 
 velocidade_Internet = float(input("Digite a velocidade da internet em Mbs: "))
 
+# Cálculo do tempo de Download
 tempo_Download = arquivo / (velocidade_Internet / 8)
 
 print(f"O tempo aproximado de download é {tempo_Download:.1f} minutos")
