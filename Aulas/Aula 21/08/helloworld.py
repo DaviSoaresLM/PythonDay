c22b41769a0ed33c8388af07356d371959a6858e
Davi Slm <davisoaresdasilva1203@gmail.com>
2023-08-21 21:17:46 -0300

    Questões

---
+++ Aulas/Aula 21/08/helloworld.py
@@ -0,0 +1,17 @@
+'''a = "Hello, world"
+print(a[1])
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+a = 'Hello, world'
+print(a.strip()) return "Hello, world"
+
+
+a = "Hello, World!"
+print(a.split(",")) #Returns '''
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(a)
