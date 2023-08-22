1446f232b47023446a09fa3f06b91710801a210d
Davi Slm <davisoaresdasilva1203@gmail.com>
2023-08-22 13:29:48 -0300

    Questões

---
+++ Python - Curso/Desafio 13/Reajuste Salarial.py
@@ -0,0 +1,9 @@
+#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de desconto.
+
+#Recebendo o valor do salário
+salario = float(input("Digite o valor do seu salário: R$"))
+
+#Valor desconto
+desconto = salario * (15/100)
+
+#Calculando
--- Python - Curso/Desafio 17/Catetos e Hipotenusa.py
+++ Python - Curso/Desafio 17/Catetos e Hipotenusa.py
@@ -2,15 +2,14 @@
 triângulo retângulo. Calcule e mostre o comprimento da hipotenusa'''
 
 import math
-
+#Recebendo valores
 cat_Oposto = float(input("Digite o valor do cateto oposto: "))
 cat_Adjacente = float(input("Digite o comprimento  do cateto adjacente: "))
 
 #Calculando hipotenusa
-hipotenusa = math.sqrt(cat_Oposto**2 + cat_Adjacente**2)
+hipotenusa = math.hypot(cat_Oposto, cat_Adjacente)
 
 #Resultado
-print("O comprimento da hipotenusa é:", hipotenusa)
-
+print(f"O comprimento da hipotenusa é:{hipotenusa:.2f}")
 
 
