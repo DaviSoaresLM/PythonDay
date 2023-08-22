d170aeeadf6e2c864efdc63023b8a20793b44a63
Davi Slm <davisoaresdasilva1203@gmail.com>
2023-08-22 15:48:18 -0300

    Questões

--- Python - Curso/Desafio 22/Analisador de Textos.py
+++ Python - Curso/Desafio 22/Analisador de Textos.py
@@ -1,16 +1,16 @@
-'''Crie um programa que leia o nome completo de uma pessoa e mostre:
-1. O nome com todas as letras Maiúsculas e Minúsculas;
-2. Quantas letras ao todo (sem considerar espaços);
-3. Quantas letras tem o primeiro nome.'''
+'''Crie um programa que leia o nome completo de uma pessoa e mostre:'''
+'''1. O nome com todas as letras Maiúsculas e Minúsculas'''
+'''2. Quantas letras ao todo (sem considerar espaços)'''
+'''3. Quantas letras tem o primeiro nome.'''
 
-#Recebendo o Nome:
+'''Recebendo o Nome:'''
 nome = str(input("Digite um nome completo: "))
 
-#Letras Maíúsculas e Minúsculas
+'''Letras Maíúsculas e Minúsculas'''
 print("Nome Maiúsculo:", nome.upper())
 print("Nome Minúsculo:", nome.lower())
 
-#Quantas letras ao todo (sem considerar espaços)
+'''Quantas letras ao todo (sem considerar espaços)'''
 quantidade_Letras = len(nome.replace(" ", ""))
 print(("Quantidade total de letras é:", quantidade_Letras))
 
