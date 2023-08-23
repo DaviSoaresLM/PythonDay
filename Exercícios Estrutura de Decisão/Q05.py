04b4b36216fe9bdf5710a300d67211a78af85b4a
Davi Slm <davisoaresdasilva1203@gmail.com>
2023-08-22 23:23:54 -0300

    Questões

--- Exercícios Estrutura de Decisão/Q05.py
+++ Exercícios Estrutura de Decisão/Q05.py
@@ -4,3 +4,17 @@ A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 A mensagem "Reprovado", se a média for menor do que sete;
 A mensagem "Aprovado com Distinção", se a média for igual a dez.'''
 
+#Recebendo as notas
+n1 = float(input("Digite a primeira nota:"))
+n2 = float(input("Digite a segunda nota:"))
+
+#Calculando a média
+media = (n1+n2)/2
+
+#Imprimindo o resultado
+if media == 10:
+    print("Aprovado com Distinção")
+elif media >= 7.0:
+    print("Aprovado")
+else:
+    print("Reprovado")
