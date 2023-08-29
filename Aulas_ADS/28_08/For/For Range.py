ef631a1f9099e26ffc1e463f543b137b4c26acd1
Davi Slm <davisoaresdasilva1203@gmail.com>
2023-08-28 21:30:17 -0300

    Questões

---
+++ Aulas_ADS/28_08/For/For Range.py
@@ -0,0 +1,26 @@
+'''frutas = ["maçã", "banana", "acerola"]
+for x in range(len(frutas)):
+    print(frutas[x])
+
+for x in range(6):
+  print(x)
+
+for x in range(2, 6):
+  print(x)
+
+for x in range(2,30,3):
+    print(x)'
+
+for x in range(6):
+    print(x)
+else:
+    print("Testando o que acontece")
+
+for x in range(6):
+    if x == 3: break
+    print(x)
+else:
+    print("Testando")'''
+
+
+
---
+++ Aulas_ADS/28_08/For/Nested Loops.py
@@ -0,0 +1,6 @@
+adj = ["red", "big", "tasty"]
+frutas = ["maçã", "banana", "cereja"]
+
+for x in adj:
+    for y in frutas:
+        print(x,y)
